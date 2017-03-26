388ecbc9753610b0acf1a79d1477e7c256c9182a
Joshua Rinard <jrinard@hotmail.com>
2017-03-25 19:10:21 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 var deckBuild = function() {
 
 var ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-var suits = ['♠', '♡', '♢', '♣'];
+var suits = ["<img class='suit'src='img/spades.png'>", "<img class='suit'src='img/hearts.png'>", "<img class='suit'src='img/diamonds.png'>", "<img class='suit'src='img/clubs.png'>"];
 
   suits.forEach(function(suit) {
    ranks.forEach(function(rank) {
-    $("ul#cards").append("<li>"+rank+" of "+suit+"</li>");
+    $("#allcards").append("<div class='single-card'>" + rank + "<div class='suit-card'>" + suit + "</div></div>");
+
    });
   });
 };
